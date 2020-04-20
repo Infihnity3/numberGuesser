a8f543dcc2749b6f0e26cc511995d377b5cfa216
ZackChow <zackchow63@gmail.com>
2020-04-20 13:03:54 +0800

    fully functional site implemented

--- script.js
+++ script.js
@@ -3,4 +3,26 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
-
+function generateTarget() {
+    return Math.floor(Math.random())
+  }
+  function compareGuesses(userGuess, computerGuess, secretTarget) {
+    if (Math.abs(userGuess) > Math.abs(computerGuess)) {
+      return true
+    } else if (Math.abs(userGuess) < Math.abs(computerGuess)) {
+      return false
+    }
+  }
+  
+  function updateScore(update){
+    if (update == 'human'){
+    humanScore += 1
+    } else if (update == 'computer'){
+      computerScore += 1
+    }
+  }
+  
+  function advanceRound(){
+    currentRoundNumber += 1
+  }
+  
